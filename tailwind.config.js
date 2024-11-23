6d580cda60d74da1829e4ee25e3435777213a244
Zanib Zubair <166975122+zanib-sheikh@users.noreply.github.com>
2024-11-23 19:38:01 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'space-grotesk': ['Space Grotesk', 'sans-serif'],  // Add the font family
+      },
+      animation: {
+        gradient: 'gradient 4s infinite',
+      },
+      keyframes: {
+        gradient: {
+          '70%': {
+            transform: 'scale(1.2) translate(50px)',
+          },
+        },
+      },
+      backdropFilter: {
+        'blur-lg': 'blur(80px)',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-filters'), // Make sure you have this plugin installed for backdrop-filter utility
+  ],
+};
